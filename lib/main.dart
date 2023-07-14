c8d62462649544f4935b8786568aa05b7fce2882
ale <alexagudilu@gmail.com>
2023-07-14 10:15:58 -0500

    segundo envio, temperatura y ciudad

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:wear/wear.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -54,13 +56,44 @@ class DanceTwo extends StatefulWidget {
   _DanceTwoState createState() => _DanceTwoState();
 }
 
+class WeatherAPI {
+  static const apiKey = '975ecea8733bd0c74f486c5ec049fe78';
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5';
+
+  static Future<Map<String, dynamic>> fetchWeatherData(String cityName) async {
+    final url = Uri.parse('$baseUrl/weather?q=$cityName&appid=$apiKey');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Fallo al conseguir datos del clima');
+    }
+  }
+}
+
 class _DanceTwoState extends State<DanceTwo> {
   late DateTime _currentTime;
+  String cityName =
+      'Mexico City'; // Nombre de la ciudad para obtener el pronóstico del clima
+  Map<String, dynamic> weatherData = {};
 
   @override
   void initState() {
     super.initState();
     _updateTime();
+    fetchWeatherData();
+  }
+
+  Future<void> fetchWeatherData() async {
+    try {
+      final data = await WeatherAPI.fetchWeatherData(cityName);
+      setState(() {
+        weatherData = data;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   void _updateTime() {
@@ -118,6 +151,24 @@ class _DanceTwoState extends State<DanceTwo> {
                 fontFamily: 'Dosis',
               ),
             ),
+            Text('Ciudad: ${weatherData['name']}',
+                style: TextStyle(
+                  color: widget.mode == WearMode.active
+                      ? Colors.black
+                      : Colors.grey[400],
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Dosis',
+                )),
+            Text('Temperatura: ${weatherData['main']['temp']}',
+                style: TextStyle(
+                  color: widget.mode == WearMode.active
+                      ? Colors.black
+                      : Colors.grey[400],
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Dosis',
+                )),
           ],
         ),
       ),
